96283d1b2f5fedf73b363a960afdd1f0641a90ac
Jagriti-yadav <jagritiyadav1901@gmail.com>
2026-09-18 23:53:35 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/smart-procurement-system/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
